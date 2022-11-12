2569e29c5b1885754699911b20ef16f895d573b3
Jean Paul Mosquera Arevalo <jean.paul@tresastronautas.com>
2022-11-12 16:00:48 -0500

    save request

--- citas-web/app.js
+++ citas-web/app.js
@@ -14,9 +14,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public-flutter')));
 
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public-flutter/index.html'));
+    //get the ip of the client
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    //get the device of the client
+    var device = req.headers['user-agent'];
+    //get the date of the request
+    var typeOfRequest = req.method;
+    var date = new Date();
+
+    //send to server with post
+    var request = require('request');
+    request.post(
+        'https://webapicitamedicaunl.azurewebsites.net/Audit',
+        { json: { auditId:0, ip: ip, dispositivo: device, tipo: typeOfRequest, fechaHora: date } },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body)
+            }
+        }
+    );
+    
 });
+
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
