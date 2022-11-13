ad8181f1e789885ba28f7e153f5818227dd458e5
Jean Paul Mosquera Arevalo <jean.paul@tresastronautas.com>
2022-11-12 21:21:56 -0500

    adding code to get header and save to server

--- citas-web/app.js
+++ citas-web/app.js
@@ -4,46 +4,66 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var app = express();
-
+var router = express();
+const expressip = require('express-ip');
+router.use(expressip().getIpInfoMiddleware);
+router.set('trust proxy', true)
 const port = 3000
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public-flutter')));
+router.use(logger('dev'));
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+router.use(cookieParser());
 
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public-flutter/index.html'));
-    //get the ip of the client
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    //get the device of the client
-    var device = req.headers['user-agent'];
-    //get the date of the request
-    var typeOfRequest = req.method;
-    var date = new Date();
+router.get('/', (req, res) => {
 
-    //send to server with post
-    var request = require('request');
-    request.post(
-        'https://webapicitamedicaunl.azurewebsites.net/Audit',
-        { json: { auditId:0, ip: ip, dispositivo: device, tipo: typeOfRequest, fechaHora: date } },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body)
-            }
+        var date =  new Date();
+        var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress ;
+        var browser =req.headers['user-agent'];
+        var method = req.method;
+
+        //json
+        var json = {
+            "ip": ip,
+            "fechaHora": date,
+            "dispositivo": browser,
+            "tipo": method
         }
-    );
-    
+
+        ///request post to api
+        var request = require('request');
+        var options = {
+            'method': 'POST',
+            'url': 'https://webapicitamedicaunl.azurewebsites.net/Audit',
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(json)
+
+        };
+        request(options, function (error, response) {
+            if (error) throw new Error(error);
+            console.log(response.body);
+        });
+
+
+        //ip
+        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        //os
+        var os = require('os');
+        var osName = os.platform();
+        var osVersion = os.release();
+        //language
+        var language = req.headers["accept-language"];
+        
+
+
+
+        res.sendFile(path.join(__dirname + '/public-flutter/index.html'));
 });
 
+router.listen(port, () => console.log(`Example router listening on port ${port}!`))
 
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-module.exports = app;
+module.exports = router;
 
